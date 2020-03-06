efb051fd2379fb286d3a379fec071c8377ff4a0a
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-06 14:36:43 -0600

    Added classes to build a input and a email text in a form

--- js/builder.js
+++ js/builder.js
@@ -2,20 +2,20 @@ class Form{
     constructor(){
         this.fields=[]
     }
-    addFields(text,type){
+    addFields(type,text){
         let fields=this.fields
         let field
 
         switch (type) {
             case 'text':
-                
+                field=new InputText(text)
                 break;
             case 'email':
-                
+                field=new InputEmail(text)
                 break;
 
             case 'button':
-                
+                field=new Button(text)
                 break;
         
             default:
@@ -32,4 +32,46 @@ class Inputs{
     }
 }
 
+class InputText extends Inputs{
+    constructor(text){
+        super(text)
+    }
+    builElement(){
+        const inputElement=document.createElement('input')
+        inputElement.setAttribute('type','text')
+        inputElement.setAttribute('placeholder',this.text)
+        return inputElement
+    }
+}
+
+
+class InputEmail extends Inputs{
+    constructor(text){
+        super(text)
+    }
+    builElement(){
+        const emailElement=document.createElement('email')
+        inputElement.setAttribute('type','email')
+        inputElement.setAttribute('placeholder',this.text)
+        return emailElement
+    }
+}
+
+class button extends Inputs{
+    constructor(text){
+        super(text)
+    }
+    builElement(){
+        const buttinElement=document.createElement('input')
+        inputElement.setAttribute('type','text')
+        inputElement.setAttribute('placeholder',this.text)
+        return buttinElement
+    }
+}
+
 const aForm=new Form()
+
+aForm.addFields('text','Insert your name')
+aForm.addFields('email','insert your email here')
+
+console.log(aForm)
