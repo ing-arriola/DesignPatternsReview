6a39d805d3912c236d94f512967e913b7da9e461
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-06 11:24:59 -0600

    Completed example with factory DP

--- js/factory.js
+++ js/factory.js
@@ -13,14 +13,43 @@ function siteBuilder(params) {
                 html=new HeadingHTML(text)
                 break;
             case 'p':
-                html= newParagraphHTML(text)
+                html= new ParagraphHTML(text)
                 break;
+        }
+        html.type=type
+
+        html.show=function(){
+            const element=document.createElement(this.type)
+            switch (this.type) {
+                case 'input':
+                    element.setAttribute('placeholder',this.text)
+                    break;
+                case 'img':
+                    element.setAttribute('src',this.text)
+                    break;
+                default:
+                    element.appendChild(document.createTextNode(this.text))
+                    break;
+            }
+            document.getElementById('app').appendChild(element)
         }
         return html
     }
 }
 
+const InputHTML=function(text){
+    this.text=text
+}
+
+const ImgHTML=function(text){
+    this.text=text
+}
+
 const HeadingHTML=function(text){
+    this.text=text
+}
+
+const ParagraphHTML=function(text){
     this.text=text
 }
 
@@ -28,5 +57,14 @@ const webSite= new siteBuilder()
 const webElements=new Array()
 
 webElements.push(webSite.createElements('Welcome to my website','h1'))
+webElements.push(webSite.createElements('Hello this is my website made with factory design pattern in JS','p'))
+webElements.push(webSite.createElements('Write any text','input'))
+webElements.push(webSite.createElements('Contact us','p'))
+webElements.push(webSite.createElements('logo.png','img'))
+
+
+webElements.forEach(element=>{
+    element.show()
+})
 
 console.log(webElements)
