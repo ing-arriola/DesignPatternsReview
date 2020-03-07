3321ca573be8c2d4ed30a123cafa3870b725fab5
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-07 11:47:53 -0600

    Added observer DP, its observers and publishers... pending documentation

--- js/observer.js
+++ js/observer.js
@@ -1,5 +1,7 @@
 let observer={
-    getOffers: function(callback) {
+    //The functions of the user now is added to the suscribers list (array)
+    //So, the suscribers array members are FUNCTIONS!! :o
+    suscribe: function(callback) {
         if (typeof callback === 'function') {
             this.suscribers[this.suscribers.length]=callback
         }
@@ -16,23 +18,24 @@ let observer={
     //Post a offer for every user
     postOffers: function(offer){
         for (let i = 0; i < this.suscribers.length; i++) {
-            if (typeof suscribers[i] === 'function') {
+            if (typeof this.suscribers[i] === 'function') {
                 this.suscribers[i](offer)
-            }
-            
+            }       
         }
 
     },
+    //Adds properties from the observer object to the vendors as well knows as observers
     createOffers: function(object){
         for (i in this) {
-            if (this.hasOwnProperty[i]) {
+            if (this.hasOwnProperty(i)) {
                 object[i]=this[i]
                 object.suscribers=[]
             }
         }
     } 
+    
 }
-//Publisher
+//Vendors
 const academy = {
     newCourse: function(){
         const course='new JS course'
@@ -46,3 +49,28 @@ const socialNetwork={
         this.postOffers(ad)
     }
 }
+
+//Create publishers... here they gets the observers properties
+observer.createOffers(academy)
+observer.createOffers(socialNetwork)
+
+const peter={
+    share: function(offer){
+        console.log(`Peter says: Wow! excellent offer ${offer}`)
+    }
+}
+
+const sam={
+    interest: function(offer){
+        console.log(`Sam says: i'm interesed on ${offer}`)
+    }
+}
+
+
+academy.suscribe(peter.share)
+academy.newCourse()
+socialNetwork.suscribe(sam.interest)
+socialNetwork.newAd()
+console.log('Sam deletes his subscription')
+socialNetwork.cancelOffers(sam.interest)
+academy.newCourse()
